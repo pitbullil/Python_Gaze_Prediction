ecc1cec5040bdf1db35de05c5df960e0c5ea7d36
pitbullil <nyarbel@campus.technion.ac.il>
2016-07-13 10:36:47 +0300

    small changes in how the batch is built

--- test.py
+++ test.py
@@ -2,16 +2,24 @@
 from skimage import io
 import dill
 import mdf_preprocessing as mpl
+import numpy as np
+import resource as rsc
+
+
 
 img_dir = '/home/user/Python_Gaze_Prediction/MSRA10K_Imgs_GT/Imgs/'
-
 gt , images = mpl.dirtomdfbatchmsra(img_dir)
 seg_dir = '/home/user/Python_Gaze_Prediction/SLIC_Segs/300/'
 mean_img = io.imread('mean_image.jpg')
-test_batch,train_batch = mpl._generate_image_segments_and_label_batch(images,img_dir,seg_dir,mean_img)
-f = open('trainy','wb')
-dill.dump(train_batch,f)
-f.close()
-f = open('testy','wb')
-dill.dump(test_batch,f)
-f.close()
+batch_size = 20
+batches = 30;#np.uint8(images.__len__()/batch_size)
+images_np = np.array(images)
+soft, hard = rsc.getrlimit(rsc.RLIMIT_AS)
+rsc.setrlimit(rsc.RLIMIT_AS, (1024*1024*1024*14, hard))
+
+for i in range(0,1):
+    path = './mdf_input/batch'+str(i)+'.bin'
+    mpl.write_batch_to_file(path,images,i,batch_size,img_dir,seg_dir,mean_img)
+    f = 0
+#test_batch = mpl._generate_image_segments_and_label_batch(images_np[shuffle_i[2000*(i+1):2000*(i+2)]],img_dir,seg_dir,mean_img)
+ 
