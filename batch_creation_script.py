9a39b5c8353760618eb3bb2cb961f8adeaf495b0
pitbullil <nyarbel@campus.technion.ac.il>
2016-07-31 12:23:13 +0300

    script for creation mdf input batches for training

--- batch_creation_script.py
+++ batch_creation_script.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from skimage import io
+from skimage import io as sio
 import dill
 import mdf_preprocessing as mpl
 import numpy as np
@@ -7,7 +7,7 @@ import resource as rsc
 
 
 
-img_dir = '/home/user/Python_Gaze_Prediction/MSRA10K_Imgs_GT/Imgs/'
+img_dir = '/home/nyarbel/Python_Gaze_Prediction/MSRA10K_Imgs_GT/Imgs/'
 gt , images = mpl.dirtomdfbatchmsra(img_dir)
 seg_dir = '/home/user/Python_Gaze_Prediction/SLIC_Segs/300/'
 mean_img = io.imread('mean_image.jpg')
@@ -21,5 +21,6 @@ for i in range(0,1):
     path = './mdf_input/batch'+str(i)+'.bin'
     mpl.write_batch_to_file(path,images,i,batch_size,img_dir,seg_dir,mean_img)
     f = 0
-#test_batch = mpl._generate_image_segments_and_label_batch(images_np[shuffle_i[2000*(i+1):2000*(i+2)]],img_dir,seg_dir,mean_img)
+#test_batch = mpl._generate_image_segments_and_label_batch(images_n
+# p[shuffle_i[2000*(i+1):2000*(i+2)]],img_dir,seg_dir,mean_img)
  
